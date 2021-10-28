b15eae7e44c7529445800dd36effa8227c38b114
LENOVO <LENOVO@DESKTOP-N2F44FC>
2021-10-28 22:39:50 +0700

    Add project files.

---
+++ LibraryUnitTest/UserTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TugasLibrary;
+using NUnit.Framework;
+
+namespace LibraryUnitTest
+{
+    [TestFixture]
+    class LibraryUserTest
+    {
+        private User user;
+
+        [SetUp]
+        public void Init()
+        {
+            user = new User();
+        }
+
+        [Test]
+        public void IsValidUserTest()
+        {
+            Assert.AreEqual(true, user.IsValidUser("admin", "admin"));
+            Assert.AreEqual(false, user.IsValidUser("adnim", "adnim"));
+        }
+    }
+}
